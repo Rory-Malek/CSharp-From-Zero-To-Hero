ce2c578612a4d2cafb2a9a5afaa930539fe6590f
Rory Malek <rjm2018grad@gmail.com>
2021-06-06 19:23:38 -0500

    I've completed homeworks 1 and 2! I've created code to accept the full name, age, height, and weight of two individuals who choose to utilize this code to calculate their BMI (sorry for the bad unit labelling for weight)! (I apologize also if anything goes wrong- I'm super unfamiliar with Git)

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,6 +7,39 @@ namespace BootCamp.Chapter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //This code accepts the name of the individual
+            Console.Write("Name:");
+            var name = Console.ReadLine();
+            //Accept the age
+            Console.Write("Age:");
+            var age = Console.ReadLine();
+            //Accept the weight- since this will be used in a calculation, you'll need to use either "int.Parse" or "double.Parse" to convert it to its respective type
+            Console.Write("Weight:");
+            var weight = float.Parse(Console.ReadLine());
+            //Accept the height
+            Console.Write("Height (cm):");
+            var height = float.Parse(Console.ReadLine()) / 100;
+
+            var BMI = weight / (height * height);
+            Console.WriteLine("Your BMI is " + BMI);
+
+
+            //This code accepts the second individual's info from here on out
+            Console.Write("Second Name:");
+            var nameSecond = Console.ReadLine();
+            //Accept the age
+            Console.Write("Second Age:");
+            var ageSecond = Console.ReadLine();
+            //Accept the weight- since this will be used in a calculation, you'll need to use either "int.Parse" or "double.Parse" to convert it to its respective type
+            Console.Write("Second Weight:");
+            var weightSecond = float.Parse(Console.ReadLine());
+            //Accept the height
+            Console.Write("Second Height (cm):");
+            var heightSecond = float.Parse(Console.ReadLine()) / 100;
+
+            var BMISecond = weightSecond / (heightSecond * heightSecond);
+            Console.WriteLine("The second person's BMI is " + BMISecond);
         }
     }
 }
